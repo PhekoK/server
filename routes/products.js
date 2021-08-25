04a80845d75de1a5eb9cac221747553b3bf79de1
Karabo Pheko <216021429@student.uj.ac.za>
2021-08-25 18:17:08 +0200

    fixed post request

--- routes/products.js
+++ routes/products.js
@@ -19,14 +19,28 @@ router.get('/:id', function(req, res, next) {
   })
 });
 
-//Request URL http:localhost:3000/products
+//Request URL http://localhost:3000/products
 // Header : Content Type: application/json
 //Request Body: {"name":"Face Cloth","price":10,"availability":"In Stock","quantity":40,"image":"https://i2.wp.com/www.yebofresh.co.za/wp-content/uploads/2021/05/image-63.jpeg?fit=300%2C300&ssl=1&is-pending-load=1","brand":"No Name","size":"1s","category":"Toiletries , Household","SKU":"NRU12780","date_added":"2021-08-22T22:31:34.792Z","__v":0}
-router.post('/', function (req, res, next) {
+/**
+ *  {
+   "name":"Toilet Paper",
+   "price":120,
+   "availability":"In Stock",
+   "quantity":40,
+   "image":"https://norwoodhome.co.za/wp-content/uploads/2020/10/1-ply-toilet-paper-1-768x768.jpg",
+   "brand":"Twinsaver",
+   "size":"48 rolls",
+   "category":"Toiletries , Household",
+   "SKU":"NRU12110"
+ }
+ */
+
+router.post('/', (req, res) => {
   Product.create(req.body, (err, data) => {
-    if(err) throw err;
+    if (err) throw err;
     res.send(data);
-  })
+  });
 });
 
 module.exports = router;
