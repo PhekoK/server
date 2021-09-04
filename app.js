4faca81422f365f24aa4f14a05011ef0858675ee
Karabo Pheko <216021429@student.uj.ac.za>
2021-09-04 10:39:48 +0200

    Connecting to mongo db atlas

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productsRouter = require('./routes/products');
 
-//const db = require('./config/db');
+const db = require('./config/db');
 
 //mongoose.connect("mongodb://localhost:27017/capstonedb",
 
@@ -19,10 +19,10 @@ var productsRouter = require('./routes/products');
 /* const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DB_CLUSTER_NAME}.fa6ux.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 */
 
-mongoose.connect("mongodb://localhost:27017/capstonedb",
+/* mongoose.connect("mongodb://localhost:27017/capstonedb",
      { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
      .then(() => { console.log('Connected to Database!!!')})
-     .catch((error) => { console.log(error) } ); 
+     .catch((error) => { console.log(error) } );  */
 
 var app = express();
 
--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER_NAME}.fa6ux.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-
+//attempt to connect to database
 const connection = mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
